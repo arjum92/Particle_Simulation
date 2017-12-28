d575dea90b4d871b188082117e2761690471bd69
Maximilian Salomon <32635839+biofreak95@users.noreply.github.com>
2017-12-28 01:12:01 +0100

    Style changes

--- Particle_Simulation/test/test_Neighbourlist.py
+++ Particle_Simulation/test/test_Neighbourlist.py
@@ -6,8 +6,8 @@ from Particle_Simulation.Neighbourlist import Neighbourlist
 Pls write true test-classes
 '''
 
-class test_System(unittest.TestCase):
 
+class test_System(unittest.TestCase):
     def test_1d(self):
         particle_positions = np.array([[1], [0], [3], [4], [0]])
         box_space = np.array([4])
@@ -27,7 +27,8 @@ class test_System(unittest.TestCase):
         print(s1.particle_neighbour_list)
 
     def test_2dsecond(self):
-        particle_positions = np.array([[0.01, 0.01], [0, 0.03], [0.03, 0.04], [0.05, 0.05], [0.09, 0.09], [0.09, 0], [0.1,0.1]])
+        particle_positions = np.array(
+            [[0.01, 0.01], [0, 0.03], [0.03, 0.04], [0.05, 0.05], [0.09, 0.09], [0.09, 0], [0.1, 0.1]])
         box_space = np.array([0.10, 0.10])
         cutoff = 0.025
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
@@ -53,14 +54,14 @@ class test_System(unittest.TestCase):
         print(s1.cell_list)
         print(s1.particle_neighbour_list)
 
-    #jit is only better, if we have 100.000 particles
+    # jit is only better, if we have 100.000 particles
     def test_Bench(self):
         particle_positions = []
-        for i in range(0,100000):
+        for i in range(0, 100000):
             xtest = np.random.rand(2)
             particle_positions.append(xtest)
         particle_positions = np.array(particle_positions)
-        box_space = np.array([1,1])
+        box_space = np.array([1, 1])
         cutoff = 0.1
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
         s1.construct_neighborlist()
@@ -68,8 +69,8 @@ class test_System(unittest.TestCase):
         print(s1.particle_neighbour_list)
 
     def test_periodicity(self):
-        particle_positions = np.array([[19,-18]], dtype=np.int32)
-        box_space = np.array([2,2], dtype=np.int32)
+        particle_positions = np.array([[19, -18]], dtype=np.int32)
+        box_space = np.array([2, 2], dtype=np.int32)
         cutoff = 1
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
         s1.construct_neighborlist()
@@ -77,8 +78,8 @@ class test_System(unittest.TestCase):
         print(s1.particle_neighbour_list)
 
     def test_list(self):
-        particle_positions = np.array([[19,-18]])
-        box_space = np.array([4,4])
+        particle_positions = np.array([[19, -18]])
+        box_space = np.array([4, 4])
         cutoff = 1
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
 
@@ -93,28 +94,29 @@ class test_System(unittest.TestCase):
 
     def test_list3d(self):
         particle_positions = np.array([[9, 6, 4]])
-        box_space = np.array([8,6,4])
+        box_space = np.array([8, 6, 4])
         cutoff = 2
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
-        print(s1.calculate_index(np.array([1,0,0])))
+        print(s1.calculate_index(np.array([1, 0, 0])))
         lol = s1.cell_neighbour_list_3D()
         print(lol)
 
     def test_benchmark(self):
         particle_positions = []
-        for i in range(0,10000000):
+        for i in range(0, 10000000):
             xtest = np.random.rand(3)
             particle_positions.append(xtest)
         particle_positions = np.array(particle_positions)
         box_space = np.array([500, 500, 500])
         cutoff = 2
         s1 = Neighbourlist(particle_positions, box_space, cutoff)
-        print(s1.calculate_index(np.array([1,0,0])))
+        print(s1.calculate_index(np.array([1, 0, 0])))
         lol = s1.cell_neighbour_list_3D()
         s1.construct_neighborlist()
         print(lol)
         print(s1.cell_list)
         print(s1.particle_neighbour_list)
+
 
 '''
 Benchmark:
