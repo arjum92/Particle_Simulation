b6e3f1b6ac47115912dcbee5ec2e0d0db50af588
Maximilian Salomon <32635839+BioFreak95@users.noreply.github.com>
2018-01-01 22:26:23 +0100

    Update test_Neighbourlist.py

--- Particle_Simulation/test/test_Neighbourlist.py
+++ Particle_Simulation/test/test_Neighbourlist.py
@@ -1,8 +1,6 @@
 import numpy as np
 import unittest
 import numpy.testing as npt
-import sys
-sys.path.append('$home/Particle_Simulation/')
 from Particle_Simulation.Neighbourlist import Neighbourlist
 
 '''
