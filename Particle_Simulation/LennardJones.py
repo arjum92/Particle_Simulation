13da577b16f460047bb13f44705f2177f8a29361
Hannes Kneiding <33654921+hkneiding@users.noreply.github.com>
2018-01-05 14:43:46 +0100

    Update LennardJones.py

--- Particle_Simulation/LennardJones.py
+++ Particle_Simulation/LennardJones.py
@@ -1,6 +1,6 @@
 import numpy as np
-from Particle import Particle
-from System import System
+from Particle_Simulation.Particle import Particle
+from Particle_Simulation.System import System
 import Neighbourlist
 
 
